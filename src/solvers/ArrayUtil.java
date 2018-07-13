48af3ecdc01bdf6dc8c08cb58fc2c67eba915b95
mhrimaz <mhrimaz@yahoo.com>
2018-07-13 15:58:18 +0430

    added ArrayUtil

---
+++ src/solvers/ArrayUtil.java
@@ -0,0 +1,44 @@
+package solvers;
+
+
+import java.util.Random;
+
+public class ArrayUtil {
+
+    private ArrayUtil(){
+
+    }
+
+    public static void shuffle(Object[] a) {
+        int n = a.length;
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    public static void swap(Object[] a, int firstIndex, int secondIndex) {
+        Object helper = a[firstIndex];
+        a[firstIndex] = a[secondIndex];
+        a[secondIndex] = helper;
+    }
+
+    public static boolean contains(Object[] array, Object target) {
+        for(Object item:array){
+            if(item.equals(target)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int findIndex(Object[] array, Object target){
+        for(int i = 0 ; i<array.length;i++){
+            if(array[i].equals(target)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
